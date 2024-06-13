e479d0fba8fd1af57bbf6e9907a4196ba3272406
Nedman <matilda@krimarc.se>
2024-06-14 01:19:37 +0200

    lagt till grunläggande layout för aboutpage

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,58 +1,98 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { ThemeContext } from '../ThemeContext.jsx';
+import { PiStarFourFill } from "react-icons/pi";
+import { FaComputer, FaCompassDrafting  } from "react-icons/fa6";
+import Lovelight from '../assets/Love-icon-light.svg';
+import Lovedark from '../assets/Love-icon-dark.svg';
 import './AboutPage.css';
 
 const AboutPage = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const { isDark } = useContext(ThemeContext);
 
-  const handleScroll = () => {
-    const scrollPosition = window.scrollY;
-    const threshold = 80; // Change index every 50 pixels
-    const newIndex = Math.floor(scrollPosition / threshold);
-
-    console.log('Scroll Position:', scrollPosition, 'New Index:', newIndex); // Debugging line
-
-
-    if (newIndex !== activeIndex && newIndex < 4) {
-      setActiveIndex(newIndex);
-    }
-  
-    if (newIndex >= 4) {
-      // If the index is 5 or more, set all containers to be inactive
-      document.querySelectorAll('.container').forEach(container => {
-        container.classList.remove('active');
-      });
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [activeIndex]);
+  const words = [
+    "Punk-culture",
+    "Painting",
+    "Late evenings",
+    "Invisible design",
+    "Grapefruit Soju",
+    "Motion design ",
+    "Matcha",
+    "Lots of whitespace",
+    "Cats",
+    "Rounded corners",
+    "Video games",
+    "Reading long books",
+    "Clean inbox",    
+  ];
 
   return (
     <div className='AboutPage'>
+      <div className='About-section'>
+      <div className='Skills-text'>
+        <div className= 'Skill-list'>
+          <div className='Skill-list-title'>
+          <FaComputer /><h3>Proficient with</h3>
+          </div>
+          <div className={`tags ${isDark ? 'dark' : ''}`}>
+            <p>Figma</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>Javascript</p>
+            <p>React</p>
+            <p>Github</p>
+          </div>
+        </div>
+        <div className= 'Skill-list'>
+        <div className='Skill-list-title'>
+        <FaCompassDrafting /><h3>Currently learning</h3>
+          </div>
+          <div className={`tags ${isDark ? 'dark' : ''}`}>
+            <p>Js.node</p>
+            <p>Adobe AI</p>
+            <p>Blender</p>
+            <p>Design Principles</p>
+          </div>
+        </div>
+      </div>
       <div className='About-text'>
-        <div className={`container ${activeIndex === 0 ? 'active' : 'inactive'}`}>
-          <p>I grew up with a Game Boy in one hand, a paintbrush in the other and a love for creativity. Little did I know that this would be my gateway into interactive design.</p>
-        </div>
-        <div className={`container ${activeIndex === 1 ? 'active' : 'inactive'}`}>
-          <p>My desire to help people initially led me to study orthotics and prosthetics, where I learned the importance of empathy and tailored solutions. Eventually, I discovered that this and my love for design could intertwine into a career in UX.</p>
-        </div>
-        <div className={`container ${activeIndex === 2 ? 'active' : 'inactive'}`}>
-          <p>I find inspiration from the work of: Robert Kurvitz, Juxtopposed, Masayoshi Sutoh, Adham Dannaway and of course my endlessly passionated classmates.</p>
-        </div>
-        <div className={`container ${activeIndex === 3 ? 'active' : 'inactive'}`}>
-          <p>Creativity is my driving force, propelling me to constantly explore and experiment with new ideas. I thrive on the excitement of trying out new things and pushing boundaries. I find joy in a myriad of design styles, from the simplicity of minimalism to the boldness of maximalism.</p>
+        <p>I grew up with a Game Boy in one hand, a paintbrush in the other and a love for creativity. Little did I know that this would be my gateway into interactive design.</p>
+        <p>My desire to help people initially led me to study orthotics and prosthetics, where I learned the importance of empathy and tailored solutions. Eventually, I discovered that this and my love for design could intertwine into a career in UX.</p>
+        <p>I find inspiration from the work of: Robert Kurvitz, Juxtopposed, Masayoshi Sutoh, Adham Dannaway and of course my endlessly passionated classmates.</p>
+        <p>Creativity is my driving force, propelling me to constantly explore and experiment with new ideas. I thrive on the excitement of trying out new things and pushing boundaries. I find joy in a myriad of design styles, from the simplicity of minimalism to the boldness of maximalism.</p>
+      </div>
+      </div>
+      <div className='Love-items'>
+        <img 
+          src={isDark ? Lovedark : Lovelight} 
+          className='Love-icon'
+        />
+        {/* Horizontal scrolling items */}
+        <div className={`horizontal-scrolling-items ${isDark ? 'dark' : ''}`}>
+          {words.map((word, index) => (
+            <div key={index} className="horizontal-scrolling-items__item">
+              {word} <PiStarFourFill />
+            </div>
+          ))}
         </div>
       </div>
-      <div className="horizontal-scrolling-items">
-      <div className="horizontal-scrolling-items__item">
-        Here is some horizontally scrolling text used for a tutorial. It will loop smoothly. &nbsp;
+      <div className='Experience-list'>
+        <div className='Experience-list-title'>
+          <h2>Experience</h2><PiStarFourFill />
+        </div>
+        <div className='Experience-card'>
+          <div className='Experience-card-header'>
+            <div className='Experience-card-header-left'>
+              <h3>Your company here</h3>
+              <h4>As your colleague</h4>
+            </div>
+            <div className='Experience-card-header-right'>
+              <h5>Anywhere with WiFi</h5>
+              <h5>AFull-time Dream Team Member</h5>
+            </div>
+          </div>
+          <p>I eagerly tackled new challenges and lent a hand to innovative projects. My main focus was supporting the team by providing fresh perspectives and injecting creativity into our work, while absorbing all the knowledge that I could. With my genuine passion for design and a sprinkle of humor, I aimed to make each day enjoyable for myself and my colleagues.</p>
+        </div>
       </div>
-      <div className="horizontal-scrolling-items__item">
-        Here is some horizontally scrolling text used for a tutorial. It will loop smoothly. &nbsp;
-      </div>
-    </div>
     </div>
   );
 };
