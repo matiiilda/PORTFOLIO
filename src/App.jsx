5ced3feed5448c254b448e494b067ea88d9456b4
Nedman <matilda@krimarc.se>
2024-07-17 17:34:20 +0200

    äntligen ordnat navigationen utan några buggar :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { ThemeProvider } from './ThemeContext';
-import ScrollToTop from './Components/ScrollToTop';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import LandingPage from './Pages/LandingPage';
@@ -13,45 +12,31 @@ import './App.css';
 
 function App() {
   const location = useLocation();
-  const footerRef = useRef(null);
 
   return (
     <ThemeProvider>
       {location.pathname === '/about' ? (
         <BackgroundWrapperAbout>
-          <Header
-            footerRef={footerRef} />
+          <Header/>
           <Routes>
-            <Route
-              path="/"
-              element={<LandingPage />} />
             <Route
               path="/about"
               element={<AboutPage />} />
-            <Route
-              path="/case:id"
-              element={<CasePage />} />
           </Routes>
-          <Footer
-            ref={footerRef}/>
+          <Footer />
         </BackgroundWrapperAbout>
       ) : (
         <BackgroundWrapper>
-          <Header
-            footerRef={footerRef} />
+          <Header />
           <Routes>
             <Route
               path="/"
               element={<LandingPage />} />
             <Route
-              path="/about"
-              element={<AboutPage />} />
-            <Route
               path="/case:id"
               element={<CasePage />} />
           </Routes>
-          <Footer
-            ref={footerRef}/>
+          <Footer />
         </BackgroundWrapper>
       )}
     </ThemeProvider>
@@ -61,7 +46,6 @@ function App() {
 export default function AppWrapper() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
       <App />
     </BrowserRouter>
   );
