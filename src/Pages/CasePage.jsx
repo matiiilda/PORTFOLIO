8633823d4de4fd721a49df79db9cf4d8d29bdbd5
Nedman <matilda@krimarc.se>
2024-08-02 21:41:47 +0200

    MOBIL EDIT KLART!

--- src/Pages/CasePage.jsx
+++ src/Pages/CasePage.jsx
@@ -1,16 +1,20 @@
 // CasePage.jsx
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import WIP from '../Components/WIP.jsx'
 
 const CasePage = () => {
   let { id } = useParams();
 
   return (
-    <div className="case-page">
-      <h1>Case Study Details</h1>
-      <p>Case ID: {id}</p> {/* Ensure this displays correctly */}
-      <p>This is a test paragraph.</p> {/* Ensure this paragraph shows up */}
-    </div>
+    <>    <div className="case-page">
+    <h1>Case Study Details</h1>
+    <p>Case ID: {id}</p> {/* Ensure this displays correctly */}
+    <p>This is a test paragraph.</p> {/* Ensure this paragraph shows up */}
+  </div>
+  <WIP />
+  </>
+
   );
 };
 
