cc41f20c9cad3aa5c723d09209992ed33989917f
Nedman <matilda@krimarc.se>
2024-08-14 13:00:12 +0200

    uppdaterat header, och finputsat, ska tackla casepage nu, testar ny metod

--- src/Components/Cases/CaseCard.jsx
+++ src/Components/Cases/CaseCard.jsx
@@ -3,21 +3,31 @@ import { Link } from "react-router-dom";
 import { PiStarFourFill } from "react-icons/pi";
 import '../../Pages/LandingPage.css';
 
-const CaseCard = ({ id, title, role, typeOfProject, content }) => (
-  <Link to={`/case/${id}`} className="block">
-    <div className='Case-card'>
-      <div className='Case-image' />
-      <div className='Case-card-text'>
-        <div className='row'>
-          <p>{role}</p><PiStarFourFill /><p>{typeOfProject}</p>
+const CaseCard = ({ id, title, role, typeOfProject, content, image }) => {
+  // Function to check if the ID is even
+  const isEven = (number) => number % 2 === 0;
+
+  // Determine the className for the Case-card div
+  const cardClassName = `Case-card${isEven(id) ? ' even' : ''}`;
+
+  return (
+    <Link to={`/case${id}`} className="block">
+      <div className={cardClassName}>
+      <div className='Case-image'>
+          <img src={image} alt={title} />
         </div>
-        <div className='row'>
-          <h3>{title}</h3>
+        <div className='Case-card-text'>
+          <div className='row'>
+            <p>{role}</p><PiStarFourFill /><p>{typeOfProject}</p>
+          </div>
+          <div className='row'>
+            <h3>{title}</h3>
+          </div>
+          <p className='content'>{content}</p>
         </div>
-        <p className='content'>{content}</p>
       </div>
-    </div>
-  </Link>
-);
+    </Link>
+  );
+};
 
 export default CaseCard;
--- src/Pages/CasePage.jsx
+++ src/Pages/CasePage.jsx
@@ -7,7 +7,8 @@ const CasePage = () => {
   let { id } = useParams();
 
   return (
-    <>    <div className="case-page">
+    <>
+    <div className="case-page">
     <h1>Case Study Details</h1>
     <p>Case ID: {id}</p> {/* Ensure this displays correctly */}
     <p>This is a test paragraph.</p> {/* Ensure this paragraph shows up */}
