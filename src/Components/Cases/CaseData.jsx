88432b50a537fd911465c5008b4afaa27311d931
Nedman <matilda@krimarc.se>
2024-10-05 23:01:03 +0200

    12

--- src/Components/Cases/CaseData.jsx
+++ src/Components/Cases/CaseData.jsx
@@ -8,6 +8,7 @@ import personasL from '../../assets/caseImages/wood/personasL.png';
 import måltavla from '../../assets/caseImages/wood/måltavla.png';
 import kassaW from '../../assets/caseImages/wood/Kassa.png';
 import kassaP from '../../assets/caseImages/wood/kundkorgp.png';
+import barbord from '../../assets/caseImages/wood/barbord.png';
 import matbordp1 from '../../assets/caseImages/wood/matbordp1.png';
 import matbordp2 from '../../assets/caseImages/wood/matbordp2.png';
 import produktsidaw from '../../assets/caseImages/wood/Pruduktsidan.png';
@@ -15,6 +16,11 @@ import startsida1 from '../../assets/caseImages/wood/startsida1.png';
 import startsida2 from '../../assets/caseImages/wood/startsida2.png';
 import startsidap from '../../assets/caseImages/wood/Startsidap.png';
 import tillvalssida from '../../assets/caseImages/wood/Tillvalssida.png';
+import produktsidan from '../../assets/caseImages/wood/Pruduktsidanp.png';
+import läsmer from '../../assets/caseImages/wood/läsmer.png';
+import bekräftelse from '../../assets/caseImages/wood/Bekräftelse.png';
+import bekräftelsep from '../../assets/caseImages/wood/bekräftelsep.png';
+
 
 export const cases = [
     {
@@ -26,13 +32,13 @@ export const cases = [
       content: "Creating a webshop for responsibly-sourced wooden tables done by the imagined company Future Wood.",
 
       introduction1:"Future Wood is an imaginary start-up company that offers customizable tables. Their goal is to bring more sustainable furniture to the market and encourage slow-living.",
-      introduction2:"In this project we followed the whole process, from idéa to finished prototype. We brainstormed marketing strategies, core branding, did user-research as well as user-interviews to then later incorporate into the finished product.",
+      introduction2:"This project encompassed the whole process in creating a new product. From idéa to a finished prototype that is ready to be handed over to the developers.",
 
-      processAndTactics1: "We began by analyzing the existing online market for furniture to understand the common layouts, visual hierarchies, and user expectations. This exploration revealed significant gaps in accessibility across many platforms, such as low contrast between text and background, overlapping text and images, and frequent non-compliance with WCAG (Web Content Accessibility Guidelines). Recognizing these shortcomings, we made accessibility a top priority in our design process to ensure that Future Wood’s platform would be both inclusive and user-friendly.",
-      processAndTactics2: "To further refine our approach, we conducted a workshop to clarify the target market for Future Wood. We then interviewed five individuals who closely matched the target demographic to gain deeper insights into their shopping habits, preferences, and values. From these interviews, three distinct personas emerged:",
-      design1: "Leveraging the insights gained from our personas, we identified five key features that would best appeal to the target market. However, due to time constraints, we recognized that it would not be feasible to incorporate all these features into the initial prototype. To address this, we created a feature prioritization map that allowed us to determine the essential features and the order in which they should be implemented. This approach ensured that we focused on the most impactful elements first while leaving room for future iterations.",
+      processAndTactics1: "We began with exploring the existing market to understand the common layouts, visual hierarchies, and user expectations. This revealed significant gaps in accessibility across many platforms, such as low contrast between text and background and frequent non-compliance with WCAG (Web Content Accessibility Guidelines). Something we made sure to test frequently during the designing process. ",
+      processAndTactics2: "Before conducting user-research we claryfied our target demographic through a workshop, and then chose five individuals to interview that suited our criteria. This gave us insights into their shopping habits, preferences, and values. From these interviews, three distinct personas emerged:",
+      design1: "By adopting the persona's perspective, we brainstormed key features for our product. To guide our prototype, we created a feature prioritization map to determine which features to implement based on impact and effort. We decided to include customization options, an inspiration gallery, and a care advice page.",
       design2: "We experimented with multiple site flow variations, conducting user testing to evaluate their effectiveness. After careful consideration, we selected the most efficient and user-friendly flow, which laid the foundation for the subsequent stages of the design. We had two main goals, to create a simple and recognizable design, and enable users to acces the product pages with one click wherever they are on the site.",
-      design3: "During the high-fidelity prototyping phase, our focus shifted to refining the visual design to ensure that it communicated exclusivity and modernity—key aspects of Future Wood’s brand identity. We dedicated significant time to testing various combinations of brand colors, aiming to create a palette that would resonate with the target audience. Additionally, we prioritized the use of ample white space to allow users to navigate the site with ease and enjoy a calm, uncluttered browsing experience. This focus on visual simplicity and clarity was essential in reinforcing the slow-living ethos that Future Wood seeks to promote.",
+      design3: "When creating hi-fi wireframes we wanted our design to communicate exclusivity and modernity—key aspects of Future Wood’s brand identity. We dedicated significant time to testing various combinations of brand colors, aiming to create a palette that would resonate with the target audience. Additionally, we prioritized the use of big images and ample white space to allow users to navigate the site with ease and enjoy a calm, uncluttered browsing experience. This focus on visual simplicity and clarity was essential in reinforcing the slow-living ethos that Future Wood seeks to promote.",
       
       upplevelsekartaD, // Dark theme image
       personasD,
@@ -47,7 +53,12 @@ export const cases = [
       startsida1,
       startsida2,
       startsidap,
-      tillvalssida
+      tillvalssida,
+      barbord,
+      produktsidan,
+      läsmer,
+      bekräftelse,
+      bekräftelsep
     },
     {
       id: "plantpal",
