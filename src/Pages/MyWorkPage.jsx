fa4bb030f9bebc80c7b9467d6bb8fd446f46ed20
Nedman <matilda@krimarc.se>
2025-03-02 18:03:18 +0100

    added pics for blender

--- src/Pages/MyWorkPage.jsx
+++ src/Pages/MyWorkPage.jsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
-import pic1 from "../assets/art/chuchu1-2.png";
-import thu1 from "../assets/art/chuchu1-3.png";
+import ReactPlayer from "react-player";
+import pic7 from "../assets/art/chuchu1-2.png";
+import thu7 from "../assets/art/chuchu1-3.png";
 import pic2 from "../assets/art/cryptopfp1 1.png";
 import thu2 from "../assets/art/cryptopfp1 2.png";
-import pic3 from "../assets/art/ldk1 1.png";
-import thu3 from "../assets/art/ldk1 2.png";
+import pic5 from "../assets/art/ldk1 1.png";
+import thu5 from "../assets/art/ldk1 2.png";
 import pic4 from "../assets/art/Inwoo3 1.png";
 import thu4 from "../assets/art/Inwoo3 2.png";
-import pic5 from "../assets/art/LEEEEZ23 1.png";
-import thu5 from "../assets/art/LEEEEZ23 2.png";
-import pic6 from "../assets/art/ren 1.png";
-import thu6 from "../assets/art/ren 2.png";
-import pic7 from "../assets/art/MTA2 1.png";
-import thu7 from "../assets/art/MTA2 2.png";
-import pic8 from "../assets/art/tear 1.png";
-import thu8 from "../assets/art/tear 2.png";
+import pic1 from "../assets/art/LEEEEZ23 1.png";
+import thu1 from "../assets/art/LEEEEZ23 2.png";
+import pic8 from "../assets/art/ren 1.png";
+import thu8 from "../assets/art/ren 2.png";
+import pic3 from "../assets/art/MTA2 1.png";
+import thu3 from "../assets/art/MTA2 2.png";
+import pic6 from "../assets/art/tear 1.png";
+import thu6 from "../assets/art/tear 2.png";
+import pic9 from "../assets/art/angel 1.png";
+import thu9 from "../assets/art/angel 2.png";
+import pic10 from "../assets/art/sushi2 1.png";
+import thu10 from "../assets/art/sushi2 3.png";
+import pic11 from "../assets/art/kitty1 1.png";
+import thu11 from "../assets/art/kitty1 2.png";
+import pic12 from "../assets/art/kitty2 1.png";
+import thu12 from "../assets/art/kitty2 2.png";
+import gif from "../assets/art/angel.gif";
 import Button from "../Components/Button/Button";
 import "./MyWorkPage.css";
 
@@ -68,7 +78,7 @@ const Gallery = ({ images, resetSelectedImage }) => {
 };
 
 const MyWorkPage = () => {
-  const [isActive, setActiveTab] = useState("all");
+  const [isActive, setActiveTab] = useState("digitalArt");
   const [resetSelectedImage, setResetSelectedImage] = useState(false);
 
   const digitalArt = [
@@ -116,13 +126,26 @@ const MyWorkPage = () => {
 
   const threeDArt = [
     {
-      src: pic1,
-      thumb: thu1,
+      src: pic9,
+      thumb: thu9,
       subHtml: "<h4>Chuchu Artwork</h4><p>Digital art by you</p>",
     },
+    {
+      src: pic10,
+      thumb: thu10,
+      subHtml: "<h4>Crypto PFP</h4><p>Crypto-themed digital portrait</p>",
+    },
+    {
+      src: pic11,
+      thumb: thu11,
+      subHtml: "<h4>LDK Piece</h4><p>LDK art example</p>",
+    },
+    {
+      src: pic12,
+      thumb: thu12,
+      subHtml: "<h4>Inwoo Art</h4><p>Artwork of Inwoo</p>",
+    },
   ];
-
-  const allArt = [...digitalArt, ...threeDArt];
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -134,14 +157,6 @@ const MyWorkPage = () => {
     <div className="my-work-page">
       <h1>My Work</h1>
       <div className="button-group">
-        <Button
-          className={`tab-button ${
-            isActive === "all" ? "active" : "notactive"
-          }`}
-          onClick={() => handleTabChange("all")}
-        >
-          All
-        </Button>
         <Button
           className={`tab-button ${
             isActive === "digitalArt" ? "active" : "notactive"
